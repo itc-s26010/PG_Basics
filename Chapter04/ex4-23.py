ad3e75f0ddd0877dbeb994dae9adec3136f24dfe
itc-s26010 <s26010@std.it-college.ac.jp>
2026-06-03 08:01:38 +0000

    第4章例題23まで

---
+++ Chapter04/ex4-23.py
@@ -0,0 +1,14 @@
+def add(x, y):
+    """
+    Returns x + y.
+    :param x: int.
+    :param y: int.
+    :returm: int sum of x and y.
+    """
+    return x + y
+
+a = 10
+b = 8
+c = add(a,b)
+
+print(c)
